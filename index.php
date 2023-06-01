a48ff028de4e408ea6f86c75b2041fd832db03da
Developer_Alejo <103768003+alejovera1y75555@users.noreply.github.com>
2023-05-31 21:39:09 -0500

    actualizacion

--- index.php
+++ index.php
@@ -6,64 +6,41 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pagina de Bienvenida</title>
-    <link rel="stylesheet" href="../asist-web/asistencia-web/estilo/estilos.css">
+    <link rel="stylesheet" href="../asistencia-web/estilo/estilos.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,400;1,300&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,400;1,300&display=swap" rel="stylesheet">
 
-<!--<style>
-        h1 {
-            text-align: center;
-        }
-        .logo {
-            display: flex;
-            justify-content: center; 
-            width: auto;
-            height: auto;
-            max-width: 100%;
-            max-height: 100%;
-        }
-        </style>
--->
+
 </head>
 
 <body>
-    <header>
-        <nav>
-            <!-- insertar logo -->
-            <img src="../sis-asistencia/imagenes/logo.png" alt="">
-            <a href="">Inicio</a>
-            <a href="">Ingresar</a>
-            <a href="">Registrar</a>
-            <a href="">Contacto</a>
-        </nav>
-    </header>
+
     <h1>BIENVENIDOS, REGISTRA TU ASISTENCIA</h1>
     <h2 id="fecha"></h2>
-    <!--<div class="logo">
-        <img src="../sis-asistencia/imagenes/logo.png" alt=""></div>
-    -->
     <div class="container">
-    <a class="acceso" href="">Ingresar al sistema</a>
-    <p class="cedula">Ingrese su Cedula</p>
-    <form action="">
-        <input type="text" placeholder="Cedula del empleado" name="txtcc">
+        <a class="acceso" href="">Ingresar al sistema</a>
+        <div class="logo">
+            <img src="../img/logo.png" alt="">
+        </div>
+        <p class="cedula">Ingrese su Cédula</p>
+        <form action="">
+            <input type="text" placeholder="Cedula del empleado" name="txtcc">
             <div class="botones">
-            <a class="entrada" href="">ENTRADA</a>
-            <a class="salida" href="">SALIDA</a>
+                <a class="entrada" href="">ENTRADA</a>
+                <a class="salida" href="">SALIDA</a>
             </div>
-    </form>
-</div>
+        </form>
+    </div>
 
     <script>
-       setInterval(() => {
-              let fecha = new Date();
-              let fechaHora = fecha.toLocaleString();
-              document.getElementById("fecha").textContent = fechaHora;
-         }, 1000);
+        setInterval(() => {
+            let fecha = new Date();
+            let fechaHora = fecha.toLocaleString();
+            document.getElementById("fecha").textContent = fechaHora;
+        }, 1000);
     </script>
 
 </body>
+
 </html>
-
-
