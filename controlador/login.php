4119d5f31dfd2d84145c611bff5a0f7108ae2d48
Developer_Alejo <103768003+alejovera1y75555@users.noreply.github.com>
2023-06-04 10:52:28 -0500

    actualizacion

--- controlador/login.php
+++ controlador/login.php
@@ -1,12 +1,12 @@
 <?php
 
 if (!empty($_POST["btningresar"])) {
-    if (!empty($POST["usuario"]) and !empty($_POST["password"])) {
+    if (!empty($_POST["usuario"]) and !empty($_POST["password"])) {
         $usuario = $_POST["usuario"];
         $password = $_POST["password"];
         $sql = $conexion->query("select * from usuario where usuario='$usuario' and password='$password' ");
         if ($sql->fetch_object()) {
-            header("location:../vista/inicio.php");
+            header("location:../inicio.php");
         } else {
             echo "<div class='alert alert-danger'>El usuario no existe</div>";
         }
